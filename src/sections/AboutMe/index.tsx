4ed4e3e78e601b102fe44e7dfb3da478ac614a9b
MuryloSaladino <murylosaladino@gmail.com>
2024-08-13 10:56:23 -0300

    feat: about text

--- src/sections/AboutMe/index.tsx
+++ src/sections/AboutMe/index.tsx
@@ -1,14 +1,23 @@
-import { Container, Typography } from "@mui/material"
+import { Container, Stack, Typography } from "@mui/material"
 import { StyledSection } from "./styles"
 
 const AboutMe = () => {
 
 
     return(
-        <StyledSection sx={{ minHeight: "100vh" }}>
+        <StyledSection>
             <Container>
-                <Typography variant="h4" color="primary.main">About Me</Typography>
-
+                <Stack gap={3}>
+                    <Typography variant="h4" color="primary.main" fontWeight={600}>About Me</Typography>
+                    <Typography variant="body1">
+                        I'm software engineering with a passion for coding in its many areas.
+                        I've studied and practiced a lot in the last years to develop myself as
+                        a web developer, and ended up learning a lot about other areas, especially
+                        IoT. It's hard to decide my favorite technology, but I have a special place 
+                        in my heart for the ones that I learned first: React in Frontend, SpringBoot 
+                        in backend and Arduino with C++ in IoT. 
+                    </Typography>
+                </Stack>
             </Container>
         </StyledSection>
     )
--- src/sections/AboutMe/styles.tsx
+++ src/sections/AboutMe/styles.tsx
@@ -2,5 +2,5 @@ import { Box, styled } from "@mui/material";
 
 export const StyledSection = styled(Box)(({ theme }) => ({
     backgroundColor: theme.palette.background.paper,
-    padding: "30px 0"
+    padding: "50px 0"
 }))
