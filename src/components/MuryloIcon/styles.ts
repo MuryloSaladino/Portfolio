79f02606eb414d606004105509afe3b3f20c0c2e
Murylo Saladino <murylosaladino@gmail.com>
2024-09-16 15:04:51 -0300

    proper styling

--- src/components/MuryloIcon/styles.ts
+++ src/components/MuryloIcon/styles.ts
@@ -4,25 +4,25 @@ export const StyledMenuItem = styled(MenuItem)(({ theme }) => ({
     backgroundColor: theme.palette.primary.dark
 }))
 
-export const StyledLink = styled('a')`
-    color: white;
-    text-decoration: none;
-
-    display: flex;
-    align-items: center;
-    gap: 8px;
-
-    transition: 0.15s;
-
-    &:hover{
-        transform: scale(1.1);
+export const StyledLink = styled("a")(() => ({
+    color: "white",
+    textDecoration: "none",
+    display: "flex",
+    alignItems: "center",
+    gap: 8,
+    transition: "0.15s",
+    
+    ':hover': {
+        tranform: "scale(1.1)"
     }
-`
+}))
+
 
 export const StyledIconButton = styled(IconButton)(({ theme }) => ({
     height: 'max-content',
     backgroundColor: theme.palette.primary.main,
     padding: 4,
+
     ':hover': {
         backgroundColor: theme.palette.secondary.main
     }
